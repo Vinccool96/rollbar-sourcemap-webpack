be97c7983d9bf82a41297bfaa29f09c89478f6e1
Vincent Girard <vinccool96@gmail.com>
2023-05-04 10:55:47 -0400

    Prepare for publish

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import type { RollbarSourceMapOptions, SimplifiedChunk } from "./RollbarSourceMap"
+import { RollbarSourceMap } from "./RollbarSourceMap"
+
+export type { RollbarSourceMapOptions, SimplifiedChunk }
+export { RollbarSourceMap }
